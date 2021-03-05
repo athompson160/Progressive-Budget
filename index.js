22f29199e4bf136227ed0a1ea73d9d1b37aaf676
Alexander Thompson <71605358+athompson160@users.noreply.github.com>
2021-03-04 17:12:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -40,3 +40,28 @@ function populateChart() {
   // copy array and reverse it
   const reversed = transactions.slice().reverse();
   let sum = 0;
+// create date labels for chart
+  const labels = reversed.map(t => {
+    const date = new Date(t.date);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  });
+
+  // create incremental values for chart
+  const data = reversed.map(t => {
+    sum += parseInt(t.value);
+    return sum;
+  });
+
+  // remove old chart if it exists
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  const ctx = document.getElementById("my-chart").getContext("2d");
+
+  myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels,
+      datasets: [
+        {
